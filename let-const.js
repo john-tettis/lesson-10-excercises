edc5c80a862ecc6aa7a419c0ef12f995b2e82813
john <johndavidtettis@gmail.com>
2021-01-26 10:41:15 -0500

    initial upload

---
+++ let-const.js
@@ -0,0 +1,22 @@
+const PI = 3.14;
+
+/*
+What is the difference between var and let?
+    Var is a global variable - it can be accessed and 
+    changed from any scope, let can only be accessed 
+    or reassigned(not redeclared like var)withing the
+    block it was declared.
+    
+What is the difference between var and const?
+    Var is globally declared while const is block
+     scope. Const can not be reassigned or redeclared.
+
+What is the difference between let and const?
+    They are both block scope. Only let can be reassigned, 
+    while neither const nor let can be redeclared.
+
+What is hoisting?
+hoisting is when the compiler declared all 'var' variables
+before the code is initialized, and then assigns a value 
+when it reaches the original declaration of the variable.
+*/
